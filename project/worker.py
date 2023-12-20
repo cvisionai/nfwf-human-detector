00cd7755674cedb0f80f7c48d1c464f87198b374
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2023-12-20 00:36:34 -0500

    cleanup

--- project/worker.py
+++ project/worker.py
@@ -16,8 +16,8 @@ def create_task(task_type):
     time.sleep(int(task_type) * 10)
     return True
 
-@celery.task(name="slice_video")
-def slice_video(fileName, start_frame, end_frame, output_filename):
+@celery.task(bind=True, name="slice_video")
+def slice_video(self, fileName, start_frame, end_frame, output_filename):
 
     file_path = os.path.join('/inputs',fileName)
 
@@ -28,13 +28,15 @@ def slice_video(fileName, start_frame, end_frame, output_filename):
     start_time = start_frame / fps
     end_time = end_frame / fps
     ffmpeg_cmd = ['ffmpeg', '-i', file_path, '-ss', str(start_time), '-t', str(end_time - start_time), '/outputs/' + output_filename]
-    #ffmpeg_cmd = ["ffmpeg", "-ss", str(start_time), "-i", input_filepath, "-t", str(end_time), "-c", "copy", output_filepath]
     try:
-        #subprocess.check_output(ffmpeg_cmd, shell=True, stderr=subprocess.STDOUT)
         subprocess.run(ffmpeg_cmd)
     except subprocess.CalledProcessError as e:
-        #self.update_state(state=states.FAILURE, meta={'error': e.output.decode('utf-8')})
-        #os.remove(file_path)
+        self.update_state(state='FAILURE', 
+            meta={'status': 'Failed',
+            'exc_type': type(e).__name__,
+            'exc_message': e.output.decode('utf-8').split('\n'),
+            'custom': '...'
+            })
         return False
 
     # Return the output filename so the API can include it in the response
@@ -44,7 +46,6 @@ def slice_video(fileName, start_frame, end_frame, output_filename):
 def run_yolo(self, video_path, confidence=0.25):
 
     video_path = os.path.join('/inputs',video_path)
-    print("Got here?")
     weights_path = 'yolov5l6.pt'
     strategy = {
         'image_size': 1280,
@@ -60,13 +61,11 @@ def run_yolo(self, video_path, confidence=0.25):
             "--project", "/outputs/results",
             "--name", 'artifacts',
             "--save-txt", # Save text output (localizations)
-            "--save-conf", #With confidences
+            "--save-conf", # With confidences
             "--nosave", # Don't save images/video
             "--device", "0"
         ]
  
-    #if type(data_path) != list:
-    #    cmd.extend(["--data", data_path])
     image_size = strategy.get('image_size',None)
     if type(image_size) == list:
         for sz in image_size:
@@ -100,7 +99,7 @@ def run_yolo(self, video_path, confidence=0.25):
         self.update_state(state='FAILURE', 
             meta={'status': 'Failed',
             'exc_type': type(e).__name__,
-            'exc_message': e.format_exc().split('\n'),
+            'exc_message': e.output.decode('utf-8').split('\n'),
             'custom': '...'
             })
         return False
